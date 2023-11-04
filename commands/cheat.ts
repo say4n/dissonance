8e7a76f507395dca34d9a7e914d12ceff492b75f
Sayan Goswami <goswami.sayan47@gmail.com>
2023-11-04 19:32:46 +0000

    command: adds a cheatsheet command.

--- commands/cheat.ts
+++ commands/cheat.ts
@@ -1,10 +1,42 @@
-import parse from "node-html-parser";
-import TurndownService from "turndown";
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { Command } from "../command.model.ts";
+import stripAnsi from "strip-ansi";
 
-const converter = new TurndownService();
 
-const repsonse = await fetch("https://cht.sh/tr");
-const root = parse(await repsonse.text());
-const pre = root.getElementsByTagName("pre")[0];
+const fetchCheatsheet = async (command: string) => {
+  const repsonse = await fetch("https://cht.sh/" + encodeURI(command), {
+    headers: new Headers({
+      "User-Agent": "fetch",
+    }),
+  });
 
-console.log(converter.turndown(pre.innerText).replaceAll(" \\# ", "\n\n"));
+  const plainText = stripAnsi(await repsonse.text())
+    .replaceAll("#", "###")
+
+  return plainText
+}
+
+
+
+export const cheatCommand: Command = {
+  metadata: new SlashCommandBuilder()
+    .setName("cheat")
+    .setDescription("fetch a cheatsheet")
+    .addStringOption((option) =>
+      option.setName("query")
+        .setDescription("search query")
+        .setRequired(true)
+    ),
+
+  callback: async (interaction: ChatInputCommandInteraction) => {
+    await interaction.deferReply({ ephemeral: true });
+
+    const query = interaction.options.getString("query") || "";
+    const cheatsheet = await (fetchCheatsheet(query))
+
+    await interaction.reply({
+      content: cheatsheet,
+      ephemeral: true,
+    })
+  },
+};
--- commands/mod.ts
+++ commands/mod.ts
@@ -3,11 +3,13 @@ import { Command } from "../command.model.ts";
 import { pingCommand } from "./ping.ts";
 import { deleteCommand } from "./delete.ts";
 import { listCommand } from "./list.ts";
+import { cheatCommand } from "./cheat.ts";
 
 const commandList = [
   pingCommand,
   deleteCommand,
   listCommand,
+  cheatCommand,
 ];
 
 export const commands = new Collection<string, Command>();
